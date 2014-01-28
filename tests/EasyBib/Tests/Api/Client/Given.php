95c10916f2093924d517453e589de3e41029d5e6
yitznewton <yitznewton@hotmail.com>
2014-01-28 11:51:29 -0500

    remove unused Given methods

--- tests/EasyBib/Tests/Api/Client/Given.php
+++ tests/EasyBib/Tests/Api/Client/Given.php
@@ -14,34 +14,6 @@ use Guzzle\Http\Client;
 
 class Given
 {
-    public function iHaveAnOauthClient(StorageInterface $storage, Client $httpClient)
-    {
-        return new Api(
-            'config-id',
-            new ClientConfig([
-                'authorize_endpoint' => 'http://foo/oauth/authorize',
-                'token_endpoint' => 'http://foo/oauth/token',
-                'client_id' => 'foo_id',
-                'client_secret' => 'bar_secret',
-            ]),
-            $storage,
-            $httpClient
-        );
-    }
-
-    public function iHaveAnApiTraverser(Api $oauthClient, Client $httpClient)
-    {
-        return new ApiTraverser($oauthClient, $httpClient);
-    }
-
-    public function iHaveATokenStore(AccessToken $accessToken)
-    {
-        $storage = new MockStorage();
-        $storage->storeAccessToken($accessToken);
-
-        return $storage;
-    }
-
     public function iHaveAnAccessToken()
     {
         return new AccessToken([
@@ -52,10 +24,5 @@ class Given
             'token_type' => 'Bearer',
             'access_token' => 'ABC123',
         ]);
-    }
-
-    public function iHaveAnAccessContext()
-    {
-        return new Context('bar', ['USER_READ', 'DATA_READ_WRITE']);
     }
 }
