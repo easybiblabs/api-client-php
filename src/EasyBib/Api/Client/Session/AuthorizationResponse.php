2b09ff4705e40a20b0deb35ac9c9e56fea6b6532
yitznewton <yitznewton@hotmail.com>
2014-01-30 11:55:48 -0500

    AuthorizationResponse

--- src/EasyBib/Api/Client/Session/AuthorizationResponse.php
+++ src/EasyBib/Api/Client/Session/AuthorizationResponse.php
@@ -2,13 +2,34 @@
 
 namespace EasyBib\Api\Client\Session;
 
+use EasyBib\Api\Client\ArrayValidator;
+
 class AuthorizationResponse
 {
+    /**
+     * @var string
+     */
+    private $code;
+
+    private static $validParams = ['code'];
+
+    public function __construct(array $params)
+    {
+        self::validate($params);
+        $this->code = $params['code'];
+    }
+
     /**
      * @return string
      */
     public function getCode()
     {
+        return $this->code;
+    }
 
+    private static function validate(array $params)
+    {
+        $validator = new ArrayValidator(self::$validParams);
+        $validator->validate($params);
     }
 }
