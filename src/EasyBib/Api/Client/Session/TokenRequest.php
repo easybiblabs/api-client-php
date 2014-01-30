6b5304a9aa4ceb2f1f15718b3623bbe04d15c223
yitznewton <yitznewton@hotmail.com>
2014-01-30 14:16:25 -0500

    flesh out TokenRequest

--- src/EasyBib/Api/Client/Session/TokenRequest.php
+++ src/EasyBib/Api/Client/Session/TokenRequest.php
@@ -2,11 +2,42 @@
 
 namespace EasyBib\Api\Client\Session;
 
+use EasyBib\Api\Client\ApiConfig;
+use Guzzle\Http\ClientInterface;
+
 class TokenRequest
 {
-    public function __construct(ApiSession $session, AuthorizationResponse $authorizationResponse)
-    {
+    const GRANT_TYPE = 'authorization_code';
 
+    /**
+     * @var \EasyBib\Api\Client\ApiConfig
+     */
+    private $config;
+
+    /**
+     * @var \Guzzle\Http\ClientInterface
+     */
+
+    private $httpClient;
+
+    /**
+     * @var AuthorizationResponse
+     */
+    private $authorizationResponse;
+
+    /**
+     * @param ApiConfig $config
+     * @param ClientInterface $httpClient
+     * @param AuthorizationResponse $authorization
+     */
+    public function __construct(
+        ApiConfig $config,
+        ClientInterface $httpClient,
+        AuthorizationResponse $authorization
+    ) {
+        $this->config = $config;
+        $this->httpClient = $httpClient;
+        $this->authorizationResponse = $authorization;
     }
 
     /**
@@ -14,5 +45,17 @@ class TokenRequest
      */
     public function send()
     {
+        $request = $this->httpClient->post('/oauth/token', [], $this->getParams());
+        $request->send();
+    }
+
+    private function getParams()
+    {
+        return [
+            'grant_type' => self::GRANT_TYPE,
+            'code' => $this->authorizationResponse->getCode(),
+            'redirect_uri' => $this->config->getParams()['redirect_url'],
+            'client_id' => $this->config->getParams()['client_id'],
+        ];
     }
 }
---
+++ tests/EasyBib/Tests/Api/Client/Session/TokenRequestTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace EasyBib\Tests\Api\Client\Session;
+
+use EasyBib\Api\Client\ApiConfig;
+use EasyBib\Api\Client\Session\AuthorizationResponse;
+use EasyBib\Api\Client\Session\TokenRequest;
+use Guzzle\Http\Client;
+use Guzzle\Http\Message\Response;
+use Guzzle\Plugin\History\HistoryPlugin;
+use Guzzle\Plugin\Mock\MockPlugin;
+
+class TokenRequestTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var string
+     */
+    private $rootUrl = 'http://data.easybib.example.com';
+
+    /**
+     * @var Client
+     */
+    private $httpClient;
+
+    /**
+     * @var ApiConfig
+     */
+    private $config;
+
+    /**
+     * @var HistoryPlugin
+     */
+    private $history;
+
+    /**
+     * @var AuthorizationResponse
+     */
+    private $authorizationResponse;
+
+    public function setUp()
+    {
+        $this->httpClient = new Client($this->rootUrl);
+
+        $mockResponses = new MockPlugin([
+            new Response(200, [], '{}'),
+        ]);
+
+        $this->history = new HistoryPlugin();
+
+        $this->httpClient->addSubscriber($mockResponses);
+        $this->httpClient->addSubscriber($this->history);
+
+        $this->config = new ApiConfig([
+            'client_id' => 'client_123',
+            'redirect_url' => 'http://myapp.example.com/',
+        ]);
+
+        $this->authorizationResponse = new AuthorizationResponse(['code' => 'ABC123']);
+    }
+
+    public function testSend()
+    {
+        $tokenRequest = new TokenRequest($this->config, $this->httpClient, $this->authorizationResponse);
+        $tokenResponse = $tokenRequest->send();
+
+        $lastRequest = $this->history->getLastRequest();
+
+        $expectedParams = [
+            'grant_type' => 'authorization_code',
+            'code' => $this->authorizationResponse->getCode(),
+            'redirect_uri' => $this->config->getParams()['redirect_url'],
+            'client_id' => $this->config->getParams()['client_id'],
+        ];
+
+        $this->assertEquals('POST', $lastRequest->getMethod());
+        $this->assertEquals($expectedParams, $lastRequest->getPostFields()->toArray());
+        $this->assertEquals($this->rootUrl . '/oauth/token', $lastRequest->getUrl());
+        // TODO assert return value
+    }
+}
