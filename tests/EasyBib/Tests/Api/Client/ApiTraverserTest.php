61693685c81adff4581ccf29e4a8c55dca467479
yitznewton <yitznewton@hotmail.com>
2014-02-06 15:13:50 -0500

    remove superfluous abstract test class

--- tests/EasyBib/Tests/Api/Client/ApiTraverserTest.php
+++ tests/EasyBib/Tests/Api/Client/ApiTraverserTest.php
@@ -7,9 +7,89 @@ use EasyBib\Api\Client\ExpiredTokenException;
 use EasyBib\Api\Client\Resource\Collection;
 use EasyBib\Api\Client\Resource\Reference;
 use EasyBib\Api\Client\Resource\Resource;
+use EasyBib\OAuth2\Client\AuthorizationCodeGrant\Authorization\AuthorizationResponse;
+use EasyBib\OAuth2\Client\AuthorizationCodeGrant\ClientConfig;
+use EasyBib\OAuth2\Client\ServerConfig;
+use EasyBib\OAuth2\Client\TokenStore;
+use Guzzle\Http\Client;
+use Guzzle\Plugin\History\HistoryPlugin;
+use Guzzle\Plugin\Mock\MockPlugin;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
 
-class ApiTraverserTest extends TestCase
+class ApiTraverserTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var Given
+     */
+    protected $given;
+
+    /**
+     * @var string
+     */
+    protected $apiBaseUrl = 'http://data.easybib.example.com';
+
+    /**
+     * @var HistoryPlugin
+     */
+    protected $history;
+
+    /**
+     * @var Client
+     */
+    protected $httpClient;
+
+    /**
+     * @var MockPlugin
+     */
+    protected $mockResponses;
+
+    /**
+     * @var TokenStore
+     */
+    protected $tokenStore;
+
+    /**
+     * @var ClientConfig
+     */
+    protected $clientConfig;
+
+    /**
+     * @var ServerConfig
+     */
+    protected $serverConfig;
+
+    /**
+     * @var AuthorizationResponse
+     */
+    protected $authorization;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->given = new Given();
+
+        $this->clientConfig = new ClientConfig([
+            'client_id' => 'client_123',
+            'redirect_url' => 'http://myapp.example.com/',
+        ]);
+
+        $this->serverConfig = new ServerConfig([
+            'authorization_endpoint' => '/oauth/authorize',
+            'token_endpoint' => '/oauth/token',
+        ]);
+
+        $this->httpClient = new Client($this->apiBaseUrl);
+        $this->mockResponses = new MockPlugin();
+        $this->history = new HistoryPlugin();
+        $this->httpClient->addSubscriber($this->mockResponses);
+        $this->httpClient->addSubscriber($this->history);
+
+        $this->tokenStore = new TokenStore(new Session(new MockArraySessionStorage()));
+        $this->authorization = new AuthorizationResponse(['code' => 'ABC123']);
+    }
+
     /**
      * @return array
      */
@@ -131,10 +211,7 @@ class ApiTraverserTest extends TestCase
         $api = new ApiTraverser($this->httpClient);
         $api->get('url placeholder');
 
-        $this->assertTrue(
-            $this->history->getLastRequest()->getHeader('Authorization')
-                ->hasValue('Bearer ' . $accessToken)
-        );
+        $this->shouldHaveHadATokenWithLastRequest($accessToken);
     }
 
     public function testGetPassesTokenInHeaderWithAuthCodeGrant()
@@ -147,10 +224,7 @@ class ApiTraverserTest extends TestCase
         $api = new ApiTraverser($this->httpClient);
         $api->get('url placeholder');
 
-        $this->assertTrue(
-            $this->history->getLastRequest()->getHeader('Authorization')
-                ->hasValue('Bearer ' . $accessToken)
-        );
+        $this->shouldHaveHadATokenWithLastRequest($accessToken);
     }
 
     public function testGetWithExpiredToken()
@@ -165,6 +239,8 @@ class ApiTraverserTest extends TestCase
 
     /**
      * @dataProvider getValidCitations
+     * @param array $citation
+     * @param array $expectedResponseResource
      */
     public function testPost(array $citation, array $expectedResponseResource)
     {
@@ -179,6 +255,8 @@ class ApiTraverserTest extends TestCase
 
     /**
      * @dataProvider getValidCitations
+     * @param array $citation
+     * @param array $expectedResponseResource
      */
     public function testPut(array $citation, array $expectedResponseResource)
     {
@@ -206,6 +284,9 @@ class ApiTraverserTest extends TestCase
         $this->shouldHaveReturnedADeletedResource($expectedResource, $response);
     }
 
+    /**
+     * @param string $httpMethod
+     */
     private function shouldHaveMadeAnApiRequest($httpMethod)
     {
         $lastRequest = $this->history->getLastRequest();
@@ -218,6 +299,10 @@ class ApiTraverserTest extends TestCase
         );
     }
 
+    /**
+     * @param array $expectedResponseArray
+     * @param Resource $resource
+     */
     private function shouldHaveReturnedAResource(
         array $expectedResponseArray,
         Resource $resource
@@ -230,6 +315,10 @@ class ApiTraverserTest extends TestCase
         );
     }
 
+    /**
+     * @param array $expectedResponseArray
+     * @param Resource $resource
+     */
     private function shouldHaveReturnedADeletedResource(
         array $expectedResponseArray,
         Resource $resource
@@ -237,26 +326,15 @@ class ApiTraverserTest extends TestCase
         $this->assertSameData($expectedResponseArray, $resource);
     }
 
+    /**
+     * @param array $expectedResponseArray
+     * @param Collection $collection
+     */
     private function shouldHaveReturnedACollection(
         array $expectedResponseArray,
         Collection $collection
     ) {
         $this->assertEquals(count($expectedResponseArray['data']), count($collection));
-    }
-
-    private function recursiveCastObject(array $array)
-    {
-        return json_decode(json_encode($array));
-    }
-
-    private function extractReferences(array $links)
-    {
-        return array_map(
-            function ($reference) {
-                return new Reference($this->recursiveCastObject($reference));
-            },
-            $links
-        );
     }
 
     /**
@@ -268,6 +346,56 @@ class ApiTraverserTest extends TestCase
         $this->assertEquals(
             $this->recursiveCastObject($expectedResponseArray['data']),
             $resource->getResponseDataContainer()->getData()
+        );
+    }
+
+    public function shouldHaveMadeATokenRequest()
+    {
+        $lastRequest = $this->history->getLastRequest();
+
+        $expectedParams = [
+            'grant_type' => 'authorization_code',
+            'code' => $this->authorization->getCode(),
+            'redirect_uri' => $this->clientConfig->getParams()['redirect_url'],
+            'client_id' => $this->clientConfig->getParams()['client_id'],
+        ];
+
+        $this->assertEquals('POST', $lastRequest->getMethod());
+        $this->assertEquals($expectedParams, $lastRequest->getPostFields()->toArray());
+        $this->assertEquals($this->apiBaseUrl . '/oauth/token', $lastRequest->getUrl());
+    }
+
+    /**
+     * @param $accessToken
+     */
+    private function shouldHaveHadATokenWithLastRequest($accessToken)
+    {
+        $this->assertTrue(
+            $this->history->getLastRequest()->getHeader('Authorization')
+                ->hasValue('Bearer ' . $accessToken)
+        );
+    }
+
+    /**
+     * @param array $array
+     * @return \stdClass
+     */
+    private function recursiveCastObject(array $array)
+    {
+        return json_decode(json_encode($array));
+    }
+
+    /**
+     * @param array $links
+     * @return array
+     */
+    private function extractReferences(array $links)
+    {
+        return array_map(
+            function ($reference) {
+                return new Reference($this->recursiveCastObject($reference));
+            },
+            $links
         );
     }
 }
